cee8427d6941f0ae92169b28b5960ab4a01bbc4d
rwill <ryanwilldev@gmail.com>
2018-01-10 15:31:55 -0600

    adds precaching of app shell assets for home page

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,34 @@
+self.addEventListener('install', event => {
+ event.waitUntil(
+   // Precache the app shell
+   caches.open('static')
+    .then(cache => {
+      cache.addAll([
+        '/',
+        '/src/js/app.js',
+        '/src/js/feed.js',
+        '/src/js/material.min.js',
+        '/src/css/app.css',
+        '/src/css/feed.css',
+        '/src/images/main-image-lg.jpg',
+        'https://fonts.googleapis.com/css?family=Roboto:400,700',
+        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+        'https://fonts.googleapis.com/icon?family=Material+Icons'
+      ]);
+    })
+  );
+});
+
 self.addEventListener('activate', event => {
   return self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request)
+      .then(cachedAsset => {
+        if (cachedAsset) return cachedAsset;
+        else return fetch(event.request);
+      })
+  );
 });
